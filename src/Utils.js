f74efd49cca9872883ff8901659c626f8bd0cbfa
Andreas Waldherr <waldherr@mobimedia.de>
2024-07-25 15:53:42 +0200

    Improved Logging

--- src/Utils.js
+++ src/Utils.js
@@ -3,3 +3,7 @@ exports.DEFAULT_TIMEOUT = 30 * 1000;
 exports.getError = (e) => {
   return e.stack || e.toString();
 }
+
+exports.log = (message) => {
+  console.log(`${new Date().toISOString()} ${message}`);
+}
