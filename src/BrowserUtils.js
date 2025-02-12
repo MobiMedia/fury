88749f985da96170b9b3a89e77e6f755229ce8cf
Johannes <goetz@mobimedia.de>
2025-02-12 13:45:18 +0100

    done

--- src/BrowserUtils.js
+++ src/BrowserUtils.js
@@ -58,7 +58,7 @@ exports.getPuppeteerParams = () => {
       headless: !debug,
       devtools: debug,
       args: ["--no-sandbox", "--no-zygote"],
-      protocolTimeout: 60 * 5 * 1000,
+      protocolTimeout: process.env.protocolTimeout || 60 * 5 * 1000,
       ignoreHTTPSErrors: true
     };
 
